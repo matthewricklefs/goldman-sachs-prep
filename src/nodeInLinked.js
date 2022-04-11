e8a8a56f543e0cfb89b3ed859d9cb75c4056a9ee
Matthew <ricklefsmatthew1003@gmail.com>
2022-04-11 10:50:10 -0400

    only one pointer, node to be deleted in a singly linked list

--- src/nodeInLinked.js
+++ src/nodeInLinked.js
@@ -1,29 +1,3 @@
-function printString(columnNum) {
-  let columnName = [];
-
-  while (columnNum > 0) {
-    let rem = columnNum % 26;
-
-    if (rem == 0) {
-      columnName.push('Z');
-      columnNum = Math.floor(columnNum / 26) - 1;
-    } else {
-      columnName.push(String.fromCharCode(rem - 1 + 'A'.charCodeAt(0)));
-      columnNum = Math.floor(columnNum / 26);
-    }
-  }
-
-  console.log(columnName.reverse().join('') + '\n');
-}
-
-printString(26);
-printString(51);
-printString(52);
-printString(80);
-printString(676);
-printString(702);
-printString(705);
-
 var head;
 
 class Node {
